8f4d9bae254d566a3576e31cdc5e49d0ae0e673f
Ilia Malfa <[Dit214166@hua.gr>
2019-01-01 20:45:51 +0200

    3 interactive activities

---
+++ app/src/main/java/com/example/android/firstassignment/Activity1.java
@@ -0,0 +1,26 @@
+package com.example.android.firstassignment;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Activity1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_1);
+
+        Button button = findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClassName("com.example.android.firstassignment","com.example.android.firstassignment.Activity2");
+                startActivity(intent);
+            }
+        });
+    }
+}
